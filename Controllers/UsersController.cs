765e9a2d5ed8184bd45126378cdec721e193e309
mong-kieu1807 <0306231397@caothang.edu.vn>
2025-09-21 20:28:46 +0700

    Cau 01 - Kieu

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,20 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using core_group_ex_01.Services;
+//Kieu
+using core_group_ex_01.Models; //su dung AppConfig
 
 namespace core_group_ex_01.Controllers
 {
     public class UsersController : Controller
     {
         private readonly IUserService _userService;
-
-        public UsersController(IUserService userService)
+        //Kieu
+        private readonly AppConfig _config;
+        
+        public UsersController(IUserService userService, AppConfig config)
         {
             _userService = userService;
+            //Kieu
+            _config = config;
         }
 
         // GET: UsersController
-        public ActionResult Index()
+        public IActionResult Index()
         {
+            //Kieu
+            //Kiem tra IP co trong danh sach IP cho phep hay khong
+            //HttpContext.Connection.RemoteIpAddress se tra ve IP cua nguoi dung dang truy cap
+            // ?. la neu khong null thi goi tiep, null thi tra ve null
+            var userIP = HttpContext.Connection.RemoteIpAddress?.ToString();
+            // ?? la neu userIP null thi gan gia tri "" cho userIP
+            if (_config.BannerIPs.Contains(userIP ?? ""))
+            {
+                ViewBag.Message = "IP của bạn bị cấm.";
+                return View("Error");
+            }
+
             var users = _userService.GetAllUsers();
             return View(users);
         }
---
+++ Models/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace core_group_ex_01.Models
+{
+    //Kieu
+    // tao class AppConfig de luu cau hinh tu file appsettings.json
+    // file appsettings.json la file text. De chuong trinh map du lieu Json -> object C# thi can tao class nay
+    public class AppConfig
+    {
+        public long MaxFileSize { get; set; }
+        public List<string> BannerIPs { get; set; } = new();
+    }
+}
