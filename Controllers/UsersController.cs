5437dd4c8cf1a0423073616132a77f975da4e582
Chung Hue Nhu <hnhu.dev@gmail.com>
2025-09-22 20:51:38 -0700

    update users.json, chuc nang hien thi ds user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using core_group_ex_01.Services;
+using core_group_ex_01.Models;
 
 namespace MyApp.Namespace
 {
     public class UsersController : Controller
     {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
         // GET: UsersController
         public ActionResult Index()
         {
-            return View();
+            var users = _userService.GetAllUsers();
+            return View(users);
         }
     }
 }
