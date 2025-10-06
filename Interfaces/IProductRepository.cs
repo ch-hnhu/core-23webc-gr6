0e14f27cf3cec7f5982bc87a9831033e1dcfbaab
mong-kieu1807 <0306231397@caothang.edu.vn>
2025-10-07 04:23:52 +0700

    Dùng AppConfig đọc file json để kết nối với database hiển thị lên giao diện #9

--- Interfaces/IProductRepository.cs
+++ Interfaces/IProductRepository.cs
@@ -7,7 +7,7 @@ namespace core_23webc_gr6.Interfaces
     public interface IProductRepository
     {
         List<Product> GetAllProducts();
-        Product? GetProductById(string id);
+        Product? GetProductById(int id);
         void AddProduct(Product product);
         void SetProducts(List<Product> products); // Thêm method này cho middleware
     }
