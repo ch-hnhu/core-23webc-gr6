970484eb3a399ca7b473ce35f3e5b36eba5ec224
Chung Hue Nhu <hnhu.dev@gmail.com>
2025-10-11 01:26:22 -0700

    Cập nhật lại các phần liên quan đến kết nối db
    Chuyển các phần liên quan đến db từ mysql sang mssql #42

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,36 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using core_23webc_gr6.Interfaces;
-using core_23webc_gr6.Models;
-using core_23webc_gr6.Repositories;
-using MySql.Data.MySqlClient;
 
 namespace core_23webc_gr6.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    public class ProductController : Controller
-    {
-        private readonly IProductRepository _productRepository;
-        public ProductController(IProductRepository productRepository)
-        {
-            _productRepository = productRepository;
-        }
-        public IActionResult Index()
-        {
-            var products = _productRepository.GetAllProducts();
-            return View(products);
-        }
-        public IActionResult Details(int id)
-        {
-            var product = _productRepository.GetProductById(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            
-            ViewData["Area"] = "";// Gửi thông tin area xuống view PNSon thêm để view biết nó đang ở area nào
+	[Area("Admin")]
+	public class ProductController : Controller
+	{
+		private readonly IProductRepository _productRepository;
+		public ProductController(IProductRepository productRepository)
+		{
+			_productRepository = productRepository;
+		}
+		public IActionResult Index()
+		{
+			var products = _productRepository.GetAllProducts();
+			return View(products);
+		}
+		public IActionResult Details(int id)
+		{
+			var product = _productRepository.GetProductById(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
 
-            return View(product);
-        }
-        
-    }
+			ViewData["Area"] = "";// Gửi thông tin area xuống view PNSon thêm để view biết nó đang ở area nào
+
+			return View(product);
+		}
+
+	}
 }
--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,102 +1,96 @@
-﻿using core_23webc_gr6.Interfaces;
+// CHNhu - 11/10/2025 - Chỉnh lại từ mysql sang mssql
+using core_23webc_gr6.Interfaces;
 using core_23webc_gr6.Models;
-using MySql.Data.MySqlClient;
-using System.Data;
+using Microsoft.Data.SqlClient;
 namespace core_23webc_gr6.Repositories
 {
-    public class ProductRepository : IProductRepository
-    {
-        //LTMKieu 
-        //LTMKieu 06/10/2025
-        private readonly string _connString;
-        public ProductRepository(IConfiguration configuration)
-        {
-            _connString = configuration.GetConnectionString("MySqlConnection") ?? "";
-        }
-        //endLTMKieu 06/10/2025
-        //NTNguyen
-        private List<Product> _products = new();
-        //endNTNguyen
-        public List<Product> GetAllProducts()
-        {
-            var products = new List<Product>();
-            using (var connection = new MySqlConnection(_connString))
-            {
-                connection.Open();
+	public class ProductRepository : IProductRepository
+	{
+		private readonly string _connString;
+		public ProductRepository(IConfiguration configuration)
+		{
+			_connString = configuration.GetConnectionString("SqlServerConnection") ?? "";
+		}
+		private List<Product> _products = new();
+		public List<Product> GetAllProducts()
+		{
+			var products = new List<Product>();
+			using (var connection = new SqlConnection(_connString))
+			{
+				connection.Open();
 
-                string query = "SELECT * FROM Products";
-                using (var comd = new MySqlCommand(query, connection))
-                using (var reader = comd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var product = new Product
-                        {
-                            ProductID = Convert.ToInt32(reader["ProductID"]),
-                            ProductName = reader["ProductName"].ToString() ?? string.Empty,
-                            CategoryID = reader["CategoryID"] as int?,
-                            Price = Convert.ToDecimal(reader["Price"]),
-                            DiscountPercentage = Convert.ToInt32(reader["DiscountPercentage"]),
-                            Stock = Convert.ToInt32(reader["Stock"]),
-                            Image = reader["Image"].ToString(),
-                            Description = reader["Description"].ToString(),
-                            Status = Convert.ToByte(reader["Status"]),
-                            CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
-                            UpdatedAt = Convert.ToDateTime(reader["UpdatedAt"])
-                        };
-                        products.Add(product);
-                    }
-                }
-            }
-            return products;
-        }
-        public Product? GetProductById(int id)
-        {
-            Product? product = null;
-            using (var connection = new MySqlConnection(_connString))
-            {
-                connection.Open();
-                string query = "SELECT * FROM products WHERE ProductID = @id";
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@id", id);
-                    using (var reader = command.ExecuteReader())
-                    {
-                        if (reader.Read())
-                        {
-                            product = new Product
-                            {
-                                ProductID = Convert.ToInt32(reader["ProductID"]),
-                                ProductName = reader["ProductName"].ToString() ?? string.Empty,
-                                CategoryID = reader["CategoryID"] as int?,
-                                Price = Convert.ToDecimal(reader["Price"]),
-                                DiscountPercentage = Convert.ToInt32(reader["DiscountPercentage"]),
-                                Stock = Convert.ToInt32(reader["Stock"]),
-                                Image = reader["Image"].ToString(),
-                                Description = reader["Description"].ToString(),
-                                Status = Convert.ToByte(reader["Status"]),
-                                CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
-                                UpdatedAt = Convert.ToDateTime(reader["UpdatedAt"])
-                            };
-                        }
-                    }
-                }
-            }
-            return product;
-        }
-        public void AddProduct(Product product)
-        {
-            _products.Add(product);
-        }
+				string query = "SELECT * FROM Products";
+				using (var comd = new SqlCommand(query, connection))
+				using (var reader = comd.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						var product = new Product
+						{
+							ProductID = Convert.ToInt32(reader["ProductID"]),
+							ProductName = reader["ProductName"].ToString() ?? string.Empty,
+							CategoryID = reader["CategoryID"] as int?,
+							Price = Convert.ToDecimal(reader["Price"]),
+							DiscountPercentage = Convert.ToInt32(reader["DiscountPercentage"]),
+							Stock = Convert.ToInt32(reader["Stock"]),
+							Image = reader["Image"].ToString(),
+							Description = reader["Description"].ToString(),
+							Status = Convert.ToByte(reader["Status"]),
+							CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
+							UpdatedAt = Convert.ToDateTime(reader["UpdatedAt"])
+						};
+						products.Add(product);
+					}
+				}
+			}
+			return products;
+		}
 
-        //NTNguyen
-        public void SetProducts(List<Product> products)
-        {
-            _products.Clear();
-            _products.AddRange(products);
-        }
-        //endNTNguyen
-        //endLTMKieu
+		public Product? GetProductById(int id)
+		{
+			Product? product = null;
+			using (var connection = new SqlConnection(_connString))
+			{
+				connection.Open();
+				string query = "SELECT * FROM Products WHERE ProductID = @id";
+				using (var command = new SqlCommand(query, connection))
+				{
+					command.Parameters.AddWithValue("@id", id);
+					using (var reader = command.ExecuteReader())
+					{
+						if (reader.Read())
+						{
+							product = new Product
+							{
+								ProductID = Convert.ToInt32(reader["ProductID"]),
+								ProductName = reader["ProductName"].ToString() ?? string.Empty,
+								CategoryID = reader["CategoryID"] as int?,
+								Price = Convert.ToDecimal(reader["Price"]),
+								DiscountPercentage = Convert.ToInt32(reader["DiscountPercentage"]),
+								Stock = Convert.ToInt32(reader["Stock"]),
+								Image = reader["Image"].ToString(),
+								Description = reader["Description"].ToString(),
+								Status = Convert.ToByte(reader["Status"]),
+								CreatedAt = Convert.ToDateTime(reader["CreatedAt"]),
+								UpdatedAt = Convert.ToDateTime(reader["UpdatedAt"])
+							};
+						}
+					}
+				}
+			}
+			return product;
+		}
 
-    }
+		public void AddProduct(Product product)
+		{
+			_products.Add(product);
+		}
+		public void SetProducts(List<Product> products)
+		{
+			_products.Clear();
+			_products.AddRange(products);
+		}
+	}
 }
+
+// endCHNhu
