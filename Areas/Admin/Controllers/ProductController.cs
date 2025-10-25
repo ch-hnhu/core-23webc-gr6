a95c1e45b2999e476bd380cda09a5e97b7e58367
PnS235 <son235mvp@gmail.com>
2025-10-26 01:07:29 +0700

    update nhỏ trang dssp admin , lấy dữ liệu sp bằng model , chỉnh sửa load ảnh

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//aras/Admin/Controllers/ProductController.cs
+using Microsoft.AspNetCore.Mvc;
 using core_23webc_gr6.Models;
 using core_23webc_gr6.Helper;
 using Microsoft.Data.SqlClient;
@@ -16,40 +17,16 @@ namespace core_23webc_gr6.Areas.Admin.Controllers
 		}
 		public IActionResult Index(int page = 1, int pageSize = 5)
 		{
-			List<Product> products = new();
+			var productInstance = new Product();
+			var allProducts = productInstance.GetAllProducts(_db);
 
-			string query = "select * from Products";
-
-			SqlConnection conn = _db.GetConnection();
-			conn.Open();
-			SqlDataReader dr = _db.ExcuteQuery(query, conn);
-			while (dr.Read())
-			{
-				products.Add(new Product
-				{
-					productId = Convert.ToInt32(dr["productId"]),
-					productName = dr["productName"].ToString() ?? string.Empty,
-					categoryId = dr["categoryId"] as int?,
-					price = Convert.ToDecimal(dr["price"]),
-					discountPercentage = Convert.ToInt32(dr["discountPercentage"]),
-					stock = Convert.ToInt32(dr["stock"]),
-					image = dr["image"].ToString(),
-					description = dr["description"].ToString(),
-					status = Convert.ToByte(dr["status"]),
-					createdAt = Convert.ToDateTime(dr["createdAt"]),
-					updatedAt = Convert.ToDateTime(dr["updatedAt"])
-				});
-			}
-			dr.Close();
-			conn.Close();
-
-			int totalProducts = products.Count();
-
+			int totalProducts = allProducts.Count;
 			int totalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+
 			if (page < 1) page = 1;
 			if (page > totalPages) page = totalPages;
 
-			var pagedProducts = products
+			var pagedProducts = allProducts
 				.Skip((page - 1) * pageSize)
 				.Take(pageSize)
 				.ToList();
