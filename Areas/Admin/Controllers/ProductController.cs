ad383481e0a387b52d08a67b99ef605f8c150650
mong-kieu1807 <0306231397@caothang.edu.vn>
2025-10-26 02:01:56 +0700

    Quay video them hinh anh

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using core_23webc_gr6.Models;
 using core_23webc_gr6.Helper;
 using Microsoft.Data.SqlClient;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
 
 namespace core_23webc_gr6.Areas.Admin.Controllers
 {
@@ -10,9 +12,11 @@ namespace core_23webc_gr6.Areas.Admin.Controllers
 	{
 		// CHNhu - 12/10/2025 - Chỉnh lại lấy dssp từ db dùng DatabaseHelper
 		private readonly DatabaseHelper _db;
-		public ProductController(DatabaseHelper db)
+		private readonly IWebHostEnvironment _env;
+		public ProductController(DatabaseHelper db,IWebHostEnvironment env )
 		{
 			_db = db;
+			_env = env;
 		}
 		public IActionResult Index(int page = 1, int pageSize = 5)
 		{
@@ -59,12 +63,78 @@ namespace core_23webc_gr6.Areas.Admin.Controllers
 
 			return View(pagedProducts);
 		}
-
+		[HttpGet]
 		public IActionResult AddProduct()
 		{
 			return View(new Products());
 		}
 
-		// : xử lý thêm sản phẩm ( tạm thời chưa thêm xử lý sau)
+		[HttpPost]
+		public async Task<ActionResult> AddProduct(Products model)
+        {
+			try
+			{
+				//lấy tất cả file từ request.Form.Files
+				var files = Request.Form.Files;
+
+				//Tạo thư mục lưu hình ảnh
+				string uploadPath = Path.Combine(_env.WebRootPath, "img", "Products");
+				if (!Directory.Exists(uploadPath))
+					Directory.CreateDirectory(uploadPath);
+				List<string> fileNames = new List<string>();
+
+				//Duyệt qua tất cả file được upload
+				foreach (var file in files)
+				{
+					if (file != null && file.Length > 0)
+					{
+						//Tạo tên file unique
+						string fileName = $"{DateTime.Now:yyyyMMdd_HHmmss}_{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+						string filePath = Path.Combine(uploadPath, fileName);
+						//Luu file
+						using (var stream = new FileStream(filePath, FileMode.Create))
+						{
+							await file.CopyToAsync(stream);
+						}
+						fileNames.Add(fileName);
+					}
+				}
+				//lưu danh sấch tên ảnh vào model (cách nhau bởi dấu phẩy)
+				model.Image = string.Join(",", fileNames);
+
+				//đường dẫn file json
+				string jsonPath = Path.Combine(_env.ContentRootPath, "Data", "product.json");
+				//Tạo thư mục Data 
+				string dataFolder = Path.Combine(_env.ContentRootPath, "Data");
+				if (!Directory.Exists(dataFolder))
+					Directory.CreateDirectory(dataFolder);
+				//đọc file json
+				List<Products> products = new();
+				if (System.IO.File.Exists(jsonPath))
+				{
+					string json = await System.IO.File.ReadAllTextAsync(jsonPath);
+					products = JsonSerializer.Deserialize<List<Products>>(json) ?? new List<Products>();
+				}
+				//Thêm sản phẩm mới
+				products.Add(model);
+				//Ghi lại Json
+				var options = new JsonSerializerOptions
+				{
+					WriteIndented = true,
+					Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+				};
+				string updated = JsonSerializer.Serialize(products, options);
+				await System.IO.File.WriteAllTextAsync(jsonPath, updated);
+
+				TempData["Success"] = "Thêm thành công";
+				return RedirectToAction("Index");
+			}
+			catch (Exception ex)
+            {
+				TempData["Error"] = "Lỗi" + ex.Message;
+				return View(model);
+            }
+
+		}
 	}
 }
